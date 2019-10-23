cb3af705b9277a668a83520c921b8bc698fe6fe0
Kislov Dmitry <kislov@easydan.com>
2019-10-23 18:41:15 +1000

    masterdoc issue: fixed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,7 @@ sys.path.insert(0, os.path.abspath('.'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'example.settings'
 django.setup()
 
+master_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = 'Django Matplotlib'
